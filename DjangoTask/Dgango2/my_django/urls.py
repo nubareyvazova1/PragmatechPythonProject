55b7560e5126169eedddfaadaf5e120abdf6d4c1
nubareyvazova1 <eyvazovanubar480@gmail.com>
2022-04-24 23:42:16 +0400

    update

--- DjangoTask/Dgango2/my_django/urls.py
+++ DjangoTask/Dgango2/my_django/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from blogs.vews import blogs
+from accounts.vews import users
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blogs/',blogs),
-    
+    path('users/',users)
 ]
