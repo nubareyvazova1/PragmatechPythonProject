49c4cfbe645c4818006a06f8a1d70ac74390a21f
nubareyvazova1 <eyvazovanubar480@gmail.com>
2022-05-02 23:08:17 +0400

    update

--- DjangoTask/Dgango2/accounts/urls.py
+++ DjangoTask/Dgango2/accounts/urls.py
@@ -2,7 +2,8 @@
 
 from django.urls import path
 from accounts.views import user_view, register
+
 urlpatterns = [
-    path('',user_view),
-    path('register/',register),
+    path('user/', user_view),
+    path('register/', register),
 ]
