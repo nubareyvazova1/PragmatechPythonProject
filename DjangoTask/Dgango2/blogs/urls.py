0e9fc06fc2cf1afb504339a71ffca3cfc21c1105
nubareyvazova1 <eyvazovanubar480@gmail.com>
2022-05-03 22:19:38 +0400

    update

--- DjangoTask/Dgango2/blogs/urls.py
+++ DjangoTask/Dgango2/blogs/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from blogs.views import blogs
+from blogs.views import blogs, blog_detail
 
 urlpatterns = [
     path('', blogs),
+    path('<int:pk>', blog_detail),
     
 ]
--- DjangoTask/Dgango2/blogs/views.py
+++ DjangoTask/Dgango2/blogs/views.py
@@ -7,3 +7,23 @@ from django.shortcuts import render
 def blogs(request):
     
     return render (request ,'myblogs.html')
+
+def blog_detail(request,pk):
+    blogs=[
+        {
+            1:{
+                'author':'Sevinc',
+                'title':'Python',
+                'publish_data':'12.4.2002',
+            },
+            2:{
+                'author':'Gulnur',
+                'title':'Django',
+                'publish_data':'15.6.2002',
+            },
+            
+        }
+    ]
+    blog = blogs[0][pk]
+    
+    return render (request, 'blog_detail.html', {'blog': blog})
